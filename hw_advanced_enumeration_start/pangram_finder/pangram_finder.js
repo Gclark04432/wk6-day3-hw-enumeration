4e483babe01443fefc96f875717ba32b9f276e94
Gary Clark <gclark04432@live.co.uk>
2020-01-08 16:30:40 +0000

    pass-pangram-tests

--- hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
+++ hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,13 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.toLowerCase();
 }
 
 PangramFinder.prototype.isPangram = function () {
-
-}
+  const isItAPanagram = this.alphabet.every((letter) => {
+    return  this.phrase.includes(letter);
+  });
+  return isItAPanagram;
+};
 
 module.exports = PangramFinder;
