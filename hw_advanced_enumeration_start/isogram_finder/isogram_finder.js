9bc421beabf20105d8cc4f2e2ab15336a735f02d
Gary Clark <gclark04432@live.co.uk>
2020-01-08 17:14:14 +0000

    pass-2-tests-in-isogram

--- hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,23 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.split('');
 }
+
+IsogramFinder.prototype.wordAsArray = function (word) {
+  const givenLetters = word.map((letter) => {
+    return letter;
+  })
+  return givenLetters;
+};
 
 IsogramFinder.prototype.isIsogram = function () {
+  wordAsArray = this.wordAsArray(this.word);
 
-}
+  const isUnique = this.word.every((letter) => {
+    return !wordAsArray.includes(letter);
+  })
+
+  return isUnique;
+
+};
 
 module.exports = IsogramFinder;
