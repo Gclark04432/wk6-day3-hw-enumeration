8afa959e5903a63f361657dabb40a050f4da71c7
Gary Clark <gclark04432@live.co.uk>
2020-01-09 10:37:20 +0000

    add anagram finder (harrison's), and add test file (don't ask) ~ Harrison x

--- hw_advanced_enumeration_start/anagram_finder/anagram_finder.js
+++ hw_advanced_enumeration_start/anagram_finder/anagram_finder.js
@@ -1,9 +1,33 @@
 const AnagramFinder = function (word) {
+  this.word = word;
+  this.letters = this.sortLetters(word);
+}
 
+AnagramFinder.prototype.sortLetters = function (word) {
+  const lowerCaseWord = word.toLowerCase();
+  const wordAsArray = lowerCaseWord.split('');
+
+  const lettersInOrderAsArray = wordAsArray.sort();
+  const lettersInOrderAsString = lettersInOrderAsArray.join('');
+  // Harrison wrote all this don't give Gary any credit ;)
+  return lettersInOrderAsString;
+}
+
+AnagramFinder.prototype.checkWordIsAnagram = function (wordToCheck) {
+  if (wordToCheck === this.word) return false;
+
+  const wordToCheckLettersInOrder = this.sortLetters(wordToCheck);
+  return wordToCheckLettersInOrder === this.letters;
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
+  const arrayOfAnagrams = otherWords.filter((word) => {
+    const shouldWordBeKept = this.checkWordIsAnagram(word);
+    
+    return shouldWordBeKept;
+  });
 
+  return arrayOfAnagrams;
 }
 
 module.exports = AnagramFinder;
